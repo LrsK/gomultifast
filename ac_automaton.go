546cc4415737a9749c01cfc3266c8140e389e088
Lars Kulseng <larskulseng@gmail.com>
2015-11-02 14:15:20 +0100

    Code cleanup. Added SearchConcurrent.

--- ac_automaton.go
+++ ac_automaton.go
@@ -1,6 +1,4 @@
-/*
-The gomultifast package allows for creation and searching of an Aho-Corasick search trie (Automaton).
-*/
+// Package gomultifast package allows for creation and searching of an Aho-Corasick search trie (Automaton).
 package gomultifast
 
 import (
@@ -8,56 +6,52 @@ import (
 	"fmt"
 )
 
-const PATTERN_MAX_LENGTH = 5000
-const PATTERN_STARTING_LENGTH = 2000
+const patternMaxLength = 5000
+const patternStartingLength = 2000
 
-// The Aho-Corasick trie (automaton)
+// Automaton contains the Aho-Corasick trie
 type Automaton struct {
-	root           *Node   // The root of the Aho-Corasick trie
-	all_nodes      []*Node // Pointers to all nodes
-	automaton_open bool    // Automaton status. If false, no more patterns can be added
-	current_node   *Node   // Pointer to current node while searching
-	position       int     // The last searched position in a chunk.
-	base_position  int     // Position of the current chunk related to whole input text
-	total_patterns int     // Total patterns in the automaton
+	root          *node   // The root of the Aho-Corasick trie
+	allNodes      []*node // Pointers to all nodes
+	open          bool    // Automaton status. If false, no more patterns can be added
+	currentNode   *node   // Pointer to current node while searching
+	position      int     // The last searched position in a chunk.
+	basePosition  int     // Position of the current chunk related to whole input text
+	totalPatterns int     // Total patterns in the automaton
 }
 
-/*
-Type that defines the callback used to handle matches
-*/
+// MatchCallback defines the callback used to handle matches
 type MatchCallback func(Match, string, string) bool
 
-/*
-Add a Pattern (search term and identifier) to an Automaton
-*/
-func (a *Automaton) Add(pattern *Pattern) (int, error) {
-	if !a.automaton_open {
+// Add a Pattern (search term and identifier) to an Automaton
+func (a *Automaton) Add(pattern *pattern) (int, error) {
+	if !a.open {
 		return -1, errors.New("Error: Closed")
 	}
 	if len(pattern.Pstring) == 0 {
 		return -1, errors.New("Error: Zero pattern")
 	}
-	if len(pattern.Pstring) > PATTERN_MAX_LENGTH {
+	if len(pattern.Pstring) > patternMaxLength {
 		return -1, errors.New("Error: Pattern too long")
 	}
 
 	n := a.root
-	var next *Node
+	var next *node
 
 	// Break down string into runes and store in nodes
 	for _, alpha := range pattern.Pstring {
 		// Does this alpha exist in the trie?
-		next = n.find_next(alpha)
+		next = n.findNext(alpha)
 		if next != nil {
 			n = next
 			continue
 		} else {
 			// The alpha was not found, create a node that leads to it
-			next = n.create_next(alpha)
+			next = n.createNext(alpha)
 			next.depth = n.depth + 1
 			n = next
 			// Register a pointer to the node in the automaton
-			a.register_node(n)
+			a.registerNode(n)
 		}
 	}
 
@@ -66,54 +60,44 @@ func (a *Automaton) Add(pattern *Pattern) (int, error) {
 		return -1, errors.New("Error: Duplicate pattern")
 	}
 	n.final = true
-	n.register_pattern(pattern)
-	a.total_patterns++
+	n.registerPattern(pattern)
+	a.totalPatterns++
 	return 0, nil
 }
 
-/*
-Returns the number of nodes in the automaton
-*/
+// NumberOfNodes returns the number of nodes in the automaton
 func (a *Automaton) NumberOfNodes() int {
-	return len(a.all_nodes)
+	return len(a.allNodes)
 }
 
-/*
-Add a pointer to a node to the automaton registry
-*/
-func (a *Automaton) register_node(node *Node) {
-	a.all_nodes = append(a.all_nodes, node)
+// Add a pointer to a node to the automaton registry
+func (a *Automaton) registerNode(node *node) {
+	a.allNodes = append(a.allNodes, node)
 }
 
-/*
-Make the brand new trie/automaton and set the root node
-*/
+// NewAutomaton makes a brand new trie/automaton and set the root node
 func NewAutomaton() *Automaton {
 	a := Automaton{}
-	a.root = node_create()
+	a.root = nodeCreate()
 
-	a.register_node(a.root)
+	a.registerNode(a.root)
 	a.reset()
 
-	a.total_patterns = 0
-	a.automaton_open = true
+	a.totalPatterns = 0
+	a.open = true
 	return &a
 }
 
-/*
-Create a new Pattern consisting of an identifier string and a string containing the search term
-*/
-func NewPattern(Ident string, Pstring string) *Pattern {
-	p := Pattern{Ident: Ident, Pstring: Pstring}
+// NewPattern creates a new Pattern consisting of an identifier string and a string containing the search term
+func NewPattern(Ident string, Pstring string) *pattern {
+	p := pattern{Ident: Ident, Pstring: Pstring}
 	return &p
 }
 
-/*
-Reset the automata. This makes the next search begin from the root node.
-*/
+// Reset the automata. This makes the next search begin from the root node.
 func (a *Automaton) reset() {
-	a.current_node = a.root
-	a.base_position = 0
+	a.currentNode = a.root
+	a.basePosition = 0
 }
 
 /*
@@ -122,19 +106,19 @@ The slice alphas is here the prefix of a search term in the trie.
 Looks for a situation where the given node has an edge that leads to an
 alpha that exists elsewhere in the trie and points to that node if found.
 */
-func (a *Automaton) set_failure(node *Node, alphas []rune) {
+func (a *Automaton) setFailure(node *node, alphas []rune) {
 	for i := 1; i < node.depth; i++ {
 		m := a.root
 		for j := i; j < node.depth && m != nil; j++ {
-			m = m.find_next(alphas[j])
+			m = m.findNext(alphas[j])
 		}
 		if m != nil {
-			node.failure_node = m
+			node.failureNode = m
 			break
 		}
 	}
-	if node.failure_node == nil {
-		node.failure_node = a.root
+	if node.failureNode == nil {
+		node.failureNode = a.root
 	}
 }
 
@@ -145,83 +129,82 @@ Makes a recursive call to reach the end of the "trie branch".
 At each iteration, look for a "failure node", i.e. the node where the search
 can continue if the end of a trie branch is hit.
 */
-func (a *Automaton) traverse_setfailure(node *Node, alphas []rune) {
-	var next *Node
+func (a *Automaton) traverseSetfailure(n *node, alphas []rune) {
+	var next *node
 
-	for _, edge := range node.outgoing {
-		alphas[node.depth] = edge.alpha
+	for _, edge := range n.outgoing {
+		alphas[n.depth] = edge.alpha
 		next = edge.next
 		// Look for failure node
-		a.set_failure(next, alphas)
+		a.setFailure(next, alphas)
 		// Recursive call reach next node in branch
-		a.traverse_setfailure(next, alphas)
+		a.traverseSetfailure(next, alphas)
 	}
 }
 
 /*
-Here we finalize the automaton by adding failure nodes and sorting the edges of the nodes to allow for binary search.
-No more search terms can be added after this is invoked.
+Finalize puts the automaton in a state where no more search terms can be added.
+Failure nodes are added and node edges are sorted to allow for binary search.
 */
 func (a *Automaton) Finalize() {
-	alphas := make([]rune, PATTERN_STARTING_LENGTH)
+	alphas := make([]rune, patternStartingLength)
 
-	a.traverse_setfailure(a.root, alphas)
+	a.traverseSetfailure(a.root, alphas)
 
-	for _, node := range a.all_nodes {
-		a.collect_all_matched_patterns(node)
-		node.sort_edges()
+	for _, node := range a.allNodes {
+		a.collectAllMatchedPatterns(node)
+		node.sortEdges()
 	}
 	// Close the automaton to prevent further additions.
-	a.automaton_open = false
+	a.open = false
 }
 
 /*
 If a node has a failure node, add the failure node's
 matched patterns to the given node's matched patterns.
 */
-func (a *Automaton) collect_all_matched_patterns(node *Node) {
-	m := node.failure_node
+func (a *Automaton) collectAllMatchedPatterns(node *node) {
+	m := node.failureNode
 	for m != nil {
-		for _, mp := range m.matched_patterns {
-			node.register_pattern(&mp)
+		for _, mp := range m.matchedPatterns {
+			node.registerPattern(&mp)
 		}
 
 		if m.final {
 			node.final = true
 		}
-		m = m.failure_node
+		m = m.failureNode
 	}
 }
 
 /*
 Search through "text" in a finished automaton. If a match is found,
 a user defined callback function is called with the searched text, and string parameter in "param".
-if keep_searching is true, the search will continue to the rest of the text after a match.
+if keepSearching is true, the search will continue to the rest of the text after a match.
 */
-func (a *Automaton) Search(text string, keep_searching bool, callback MatchCallback, param string) int {
+func (a *Automaton) Search(text string, keepSearching bool, callback MatchCallback, param string) (bool, error) {
 	position := 0
-	var current *Node
-	var next *Node
+	var current *node
+	var next *node
 	var match Match
 
-	if a.automaton_open == true {
-		/* automaton not ready for some reason */
-		return -1
+	if a.open == true {
+		// automaton not ready for some reason
+		return false, errors.New("Automaton not ready. Must be Finalized.")
 	}
 
-	if !keep_searching {
+	if !keepSearching {
 		a.reset()
 	}
 
-	current = a.current_node
+	current = a.currentNode
 
 	// Search for the string in text, character by character
 	for position < len(text) {
-		var alpha rune = rune(text[position])
-		next = current.binary_search_next(alpha)
+		next = current.binarySearchNext(rune(text[position]))
 		if next == nil {
-			if current.failure_node != nil {
-				current = current.failure_node
+			if current.failureNode != nil {
+				current = current.failureNode
 			} else {
 				position++
 			}
@@ -234,12 +217,12 @@ func (a *Automaton) Search(text string, keep_searching bool, callback MatchCallb
 			/* We check 'next' to find out if we came here after a alphabet
 			 * transition or due to a fail. in second case we should not report
 			 * matching because it was reported in previous node */
-			match.position = position + a.base_position
-			match.Patterns = current.matched_patterns
+			match.position = position + a.basePosition
+			match.Patterns = current.matchedPatterns
 			// Match found run callback
-			cb_res := callback(match, text, param)
-			if cb_res {
-				return 1
+			cbRes := callback(match, text, param)
+			if cbRes {
+				return true, nil
 			}
 		}
 		if position >= len(text) {
@@ -248,23 +231,73 @@ func (a *Automaton) Search(text string, keep_searching bool, callback MatchCallb
 	}
 
 	// Save status variables in case we want to keep searching
-	a.current_node = current
-	a.base_position += position
+	a.currentNode = current
+	a.basePosition += position
 
-	return 0
+	return false, nil
 }
 
 /*
-Print out the automaton for debugging purposes.
+SearchConcurrent searches through "text" in a finished automaton. If a match is found,
+a user defined callback function is called with the searched text, and string parameter in "param".
+This function does not support using the state of the automaton, but can start at any position in the text input.
 */
+func (a *Automaton) SearchConcurrent(text string, position int, callback MatchCallback, param string) (bool, error) {
+	var current *node
+	var next *node
+	var match Match
+	basePosition := 0
+
+	if a.open == true {
+		// automaton not ready for some reason
+		return false, errors.New("Automaton not ready. Must be Finalized.")
+	}
+
+	current = a.root
+
+	// Search for the string in text, character by character
+	for position < len(text) {
+		next = current.binarySearchNext(rune(text[position]))
+		if next == nil {
+			if current.failureNode != nil {
+				current = current.failureNode
+			} else {
+				position++
+			}
+		} else {
+			current = next
+			position++
+		}
+
+		if current.final && next != nil {
+			/* We check 'next' to find out if we came here after a alphabet
+			 * transition or due to a fail. in second case we should not report
+			 * matching because it was reported in previous node */
+			match.position = position + basePosition
+			match.Patterns = current.matchedPatterns
+			// Match found run callback
+			cbRes := callback(match, text, param)
+			if cbRes {
+				return true, nil
+			}
+		}
+		if position >= len(text) {
+			break
+		}
+	}
+
+	return false, nil
+}
+
+// Print out the automaton for debugging purposes.
 func (a *Automaton) Print() {
-	var sid Pattern
+	var sid pattern
 
 	fmt.Printf("---------------------------------\n")
-	for _, n := range a.all_nodes {
+	for _, n := range a.allNodes {
 		var fid int
-		if n.failure_node != nil {
-			fid = n.failure_node.id
+		if n.failureNode != nil {
+			fid = n.failureNode.id
 		} else {
 			fid = 1
 		}
@@ -274,10 +307,10 @@ func (a *Automaton) Print() {
 			fmt.Printf("%c)---", e.alpha)
 			fmt.Printf("--> NODE(%3d)\n", e.next.id)
 		}
-		if len(n.matched_patterns) != 0 {
+		if len(n.matchedPatterns) != 0 {
 			fmt.Printf("Accepted patterns: {")
-			for j := 0; j < len(n.matched_patterns); j++ {
-				sid = n.matched_patterns[j]
+			for j := 0; j < len(n.matchedPatterns); j++ {
+				sid = n.matchedPatterns[j]
 				if j != 0 {
 					fmt.Printf(", ")
 				}
--- ac_node.go
+++ ac_node.go
@@ -3,38 +3,38 @@ package gomultifast
 import "sort"
 
 // used as node id number, used to count nodes
-var Node_id = 0
+var nodeID = 0
 
-// An edge indicating the next node
-type Edge struct {
+// edge indicating the next node
+type edge struct {
 	alpha rune  // Edge alpha. An alpha is a text character in the trie
-	next  *Node // Target node of the edge
+	next  *node // Target node of the edge
 }
 
 // A pattern to be stored in the trie
-type Pattern struct {
+type pattern struct {
 	Pstring string // String to add to trie
 	Ident   string // String identifier
 }
 
 // A node in the trie structure
-type Node struct {
-	id               int       // Node id
-	final            bool      // Is this a "final" node, meaning this node is the endpoint of a search
-	failure_node     *Node     // The "failure node", i.e. a node where the search can continue in case of a failed search
-	depth            int       // Distance between this node and the root
-	matched_patterns []Pattern // Slice of matched patterns at a node
-	outgoing         []Edge    // Slice of outgoing edges
+type node struct {
+	id              int       // Node id
+	final           bool      // Is this a "final" node, meaning this node is the endpoint of a search
+	failureNode     *node     // The "failure node", i.e. a node where the search can continue in case of a failed search
+	depth           int       // Distance between this node and the root
+	matchedPatterns []pattern // Slice of matched patterns at a node
+	outgoing        []edge    // Slice of outgoing edges
 }
 
-// Matches with some details
+// Match contains all found matches with some details
 type Match struct {
-	Patterns []Pattern // Slice containing matched patterns in the text
+	Patterns []pattern // Slice containing matched patterns in the text
 	position int       // The end position of matching patterns in the text
 }
 
 // Alphabetical implements sort.Interface for []Edge based on alphabetical position of Edge.alpha
-type Alphabetical []Edge
+type Alphabetical []edge
 
 func (e Alphabetical) Len() int           { return len(e) }
 func (e Alphabetical) Swap(i, j int)      { e[i], e[j] = e[j], e[i] }
@@ -43,7 +43,7 @@ func (e Alphabetical) Less(i, j int) bool { return e[i].alpha < e[j].alpha }
 /*
 If found, return next node lead to by an edge with a given alpha
 */
-func (nd *Node) find_next(alpha rune) *Node {
+func (nd *node) findNext(alpha rune) *node {
 	for _, edge := range nd.outgoing {
 		if edge.alpha == alpha {
 			return edge.next
@@ -52,79 +52,63 @@ func (nd *Node) find_next(alpha rune) *Node {
 	return nil
 }
 
-/*
-If the alpha does not yet exist, return pointer to a new node with an outgoing edge with the given alpha
-*/
-func (nd *Node) create_next(alpha rune) *Node {
-	next := nd.find_next(alpha)
+// If the alpha does not yet exist, return pointer to a new node with an outgoing edge with the given alpha
+func (nd *node) createNext(alpha rune) *node {
+	next := nd.findNext(alpha)
 	if next != nil {
 		// The edge already exists
 		return nil
 	}
 	// Otherwise register new node and edge
-	next = node_create()
-	nd.register_outgoing_edge(next, alpha)
+	next = nodeCreate()
+	nd.registerOutgoingEdge(next, alpha)
 
 	return next
 }
 
-/*
-Make a new Edge with an alpha and a pointer to a node
-*/
-func (nd *Node) register_outgoing_edge(next *Node, alpha rune) {
-	new_edge := Edge{alpha: alpha, next: next}
-	nd.outgoing = append(nd.outgoing, new_edge)
+// Make a new Edge with an alpha and a pointer to a node
+func (nd *node) registerOutgoingEdge(next *node, alpha rune) {
+	newEdge := edge{alpha: alpha, next: next}
+	nd.outgoing = append(nd.outgoing, newEdge)
 }
 
-/*
-Make a new node, and return a pointer to it
-*/
-func node_create() *Node {
-	nd := Node{id: Node_id}
-	Node_id++
+// Make a new node, and return a pointer to it
+func nodeCreate() *node {
+	nd := node{id: nodeID}
+	nodeID++
 	return &nd
 }
 
-/*
-Check if Pattern exists in a given node
-*/
-func (nd *Node) has_pattern(new_pattern *Pattern) bool {
-	for _, mp := range nd.matched_patterns {
-		if mp.Pstring == new_pattern.Pstring {
+// Check if Pattern exists in a given node
+func (nd *node) hasPattern(newPattern *pattern) bool {
+	for _, mp := range nd.matchedPatterns {
+		if mp.Pstring == newPattern.Pstring {
 			return true
 		}
 	}
 	return false
 }
 
-/*
-If pattern doesn't already exist in the node, add it
-*/
-func (nd *Node) register_pattern(new_pattern *Pattern) {
+// If pattern doesn't already exist in the node, add it
+func (nd *node) registerPattern(newPattern *pattern) {
 	// Check if the new pattern already exists in the node
-	if nd.has_pattern(new_pattern) {
+	if nd.hasPattern(newPattern) {
 		return
 	}
-	nd.matched_patterns = append(nd.matched_patterns, *new_pattern)
+	nd.matchedPatterns = append(nd.matchedPatterns, *newPattern)
 }
 
-/*
-Sort the outgoing edges of a node alphabetically to enable binary search
-*/
-func (nd *Node) sort_edges() {
+// Sort the outgoing edges of a node alphabetically to enable binary search
+func (nd *node) sortEdges() {
 	sort.Sort(Alphabetical(nd.outgoing))
 }
 
-/*
-Perform a binary search for a given alpha among the outgoing edges of a node
-*/
-func (nd *Node) binary_search_next(alpha rune) *Node {
+// Perform a binary search for a given alpha among the outgoing edges of a node
+func (nd *node) binarySearchNext(alpha rune) *node {
 	i := sort.Search(len(nd.outgoing), func(i int) bool { return nd.outgoing[i].alpha >= alpha })
 	if i < len(nd.outgoing) && nd.outgoing[i].alpha == alpha {
 		// alpha was found
 		return nd.outgoing[i].next
-	} else {
-		// alpha was not found
-		return nil
 	}
+	return nil
 }
